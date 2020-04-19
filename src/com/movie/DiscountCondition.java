894865aeb944d32b901095eaae7f766d382220c8
mjk <myeongjikang910814@gamil.com>
2020-04-19 23:06:25 +0900

    오브젝트...

--- src/com/movie/DiscountCondition.java
+++ src/com/movie/DiscountCondition.java
@@ -3,27 +3,6 @@ package com.movie;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
-public class DiscountCondition {
-    private DiscountConditionType type;
-    private int sequence;
-    private DayOfWeek dayOfWeek;
-    private LocalTime startTime;
-    private LocalTime endTime;
-
-    boolean isSatisfiedBy(Screening screening) {
-        if (type == DiscountConditionType.PERIOD) {
-            return isSatisfiedByPeriod(screening);
-        }
-        return isSatisfiedBySequence(screening);
-    }
-
-    private boolean isSatisfiedBySequence(Screening screening) {
-        return sequence == screening.getSequence();
-    }
-
-    private boolean isSatisfiedByPeriod(Screening screening) {
-        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
-                startTime.compareTo(screening.getWhenScreened().toLocalTime()) <= 0 ;
-                //endTime.isAfter(screening.getWhenScreened().toLocalTime()) >= 0;
-    }
+public interface DiscountCondition {
+    boolean isSatisfiedBy(Screening screening);
 }
--- src/com/movie/Movie.java
+++ src/com/movie/Movie.java
@@ -3,15 +3,12 @@ package com.movie;
 import java.time.Duration;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private DiscountPolicy discountPolicy;
     private List<DiscountCondition> discountConditions;
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
 
     public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
@@ -24,45 +21,7 @@ public class Movie {
         return fee;
     }
 
-    /*public Money calculateMovieFee(Screening screening) {
-        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
-    }*/
     public Money calculateMovieFee(Screening screening) {
-        if (isDiscountable(screening)) {
-            return fee.minus(calculateDiscountAmount());
-        }
-        return fee;
-    }
-
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-        }
-        throw new IllegalStateException();
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
-
-    private boolean isDiscountable(Screening screening) {
-        return discountConditions.stream().anyMatch(discountCondition -> discountCondition.isSatisfiedBy(screening));
-    }
-
-    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
